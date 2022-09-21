2d22e49afe9548ad1104a89a1801d51a56f830c4
thenns <samm77@gmail.com>
2022-09-20 22:40:12 -0600

    finished part 4/interactive

---
+++ profile.js
@@ -0,0 +1,14 @@
+//this is for profile.html
+let favColorButton = document.querySelector("#color");
+let favPlaceButton = document.querySelector("#place");
+let favRitualButton = document.querySelector("#ritual");
+
+favColorButton.addEventListener("click", function () {
+    alert("my favorite color is red");
+});
+favPlaceButton.addEventListener("click", function () {
+    alert("my favorite place is san diego");
+});
+favRitualButton.addEventListener("click", function () {
+    alert("my favorite ritual is meditation");
+});
