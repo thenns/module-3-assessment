f7dc41a1e0ff8678caf8c0be08802a52d7d94ec4
thenns <samm77@gmail.com>
2022-09-20 22:31:58 -0600

    finished part 3/html page

--- about.js
+++ about.js
@@ -1,14 +1,7 @@
-//console.log("hello world");
-
-
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
     alert("the form has been submitted");
 }
-
-
 let form = document.querySelector('#contact');
 form.addEventListener('submit', handleSubmit);
-//mouse over event
-//addEventListener('
