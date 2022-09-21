5be831c2a1b365672c95841a338e25c43bf8a51c
thenns <samm77@gmail.com>
2022-09-21 09:53:39 -0600

    cleaned up code

--- neighborhood.js
+++ neighborhood.js
@@ -1,7 +1,8 @@
 //this is for neighborhood.html
+//array of restaurants
 let restArr = [`Lomito's`, `Joe's Cafe`, `Kohinoor`, `Asa Ramen`];
 let randomRestaurant = document.querySelector("#randomRestaurant");
-
+//return a random restaurant from array
 randomRestaurant.addEventListener("click", function () {
     let rand = Math.floor(Math.random() * restArr.length);
     alert('you can try ' + restArr[rand] + '!');
